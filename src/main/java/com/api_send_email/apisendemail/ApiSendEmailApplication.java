a3f5dc672f14593e38acdd709675aa794af5ed6a
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-05-25 16:19:53 -0300

    main

---
+++ src/main/java/com/api_send_email/apisendemail/ApiSendEmailApplication.java
@@ -0,0 +1,13 @@
+package com.api_send_email.apisendemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSendEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSendEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api_send_email/apisendemail/ApiSendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_send_email.apisendemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSendEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
