74a11b6d7a290e790518900816879a30cb48598c
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-01-27 21:46:42 -0300

    caso o tipo de mensagem for vazio ele envia mensagem de portfolio

--- src/main/java/com/api_send_email/apisendemail/Controller/SendEmailApiController.java
+++ src/main/java/com/api_send_email/apisendemail/Controller/SendEmailApiController.java
@@ -2,6 +2,7 @@ package com.api_send_email.apisendemail.Controller;
 
 import com.api_send_email.apisendemail.Request.EmailSendRequest;
 import com.api_send_email.apisendemail.Services.SendEmail;
+import com.api_send_email.apisendemail.enums.TypeMessage;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins= "*", allowedHeaders= "*")
@@ -16,13 +17,19 @@ public class SendEmailApiController {
 
     @PostMapping("/send")
     public String sendEmail(@RequestBody EmailSendRequest emailSendRequest) {
+        TypeMessage typeMessage = emailSendRequest.getTypeMessage();
+
+        if (typeMessage == null){
+            typeMessage = TypeMessage.PORTFOLIO;
+        }
+
         sendEmail.send(
                 emailSendRequest.getEmailRequest().getMessage(),
                 emailSendRequest.getEmailRequest().getContacts(),
                 emailSendRequest.getEmailRequest().getSubject(),
                 emailSendRequest.getEmailRequest().getTitle(),
                 emailSendRequest.getEmailRequest().getNameProjectOrNameBusiness(),
-                emailSendRequest.getTypeMessage()
+                typeMessage
         );
 
 
